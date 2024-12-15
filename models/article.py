f82a860cd8878adfb95f841fe08ba740a05bd481
Godswillomuka <omukagodswil@gmail.com>
2024-12-15 21:51:32 +0300

    Created Magazine initialization and property methods.

--- models/article.py
+++ models/article.py
@@ -1,10 +1,10 @@
-# class Article:
-#     def __init__(self, id, title, content, author_id, magazine_id):
-#         self.id = id
-#         self.title = title
-#         self.content = content
-#         self.author_id = author_id
-#         self.magazine_id = magazine_id
+class Article:
+    def __init__(self, id, title, content, author_id, magazine_id):
+        self.id = id
+        self.title = title
+        self.content = content
+        self.author_id = author_id
+        self.magazine_id = magazine_id
 
-#     def __repr__(self):
-#         return f"Article({self.id}, '{self.title}', '{self.content}', {self.author_id}, {self.magazine_id})"
+    def __repr__(self):
+        return f"Article({self.id}, '{self.title}', '{self.content}', {self.author_id}, {self.magazine_id})"
